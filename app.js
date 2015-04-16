a55a1a04e27a1176ecd400618f388ef8c182e7c6
mishra <tech@golivelabs.io>
2015-04-16 16:06:27 -0700

    info

---
+++ app.js
@@ -0,0 +1,2 @@
+
+angular.module('calendarApp', ['calendarApp.calendar']);
---
+++ calendar/calendar-directive.js
@@ -0,0 +1,6 @@
+
+angular.module('calendar.directives', []).directive('calendar', function () {
+    return {
+        templateUrl: 'partials/_calendar.html'
+    }
+});
